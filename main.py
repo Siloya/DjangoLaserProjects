c67bf88bfacbbaec4ba44122c682bf58c6af7b47
Rachad <rachadaccoumeh@gmailcom>
2023-01-16 09:07:08 +0200

    use .env

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import time
 from RequestProvider import RequestProvider
 from RequestProvidersScheduler import RequestProvidersScheduler
 from threading import Thread
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-news_api = RequestProvider("https://newsapi.org/v2/top-headlines?country=us&apiKey=", "0a3682d43c4b47eab54374b1b5f2fc19", 20)
-news_api1 = RequestProvider("https://newsapi.org/v2/top-headlines?country=us&apiKey=", "0a3682d43c4b47eab54374b1b5f2fc19", 10)
+news_api = [
+    RequestProvider(os.getenv("HOST"), os.getenv("TOKEN"), int(os.getenv("ALLOWED_REQUEST_PER_DAY"))),
+]
 
 if __name__ == "__main__":
-    scheduler = RequestProvidersScheduler([news_api,news_api1], True)
+    scheduler = RequestProvidersScheduler(news_api, True)
     scheduler_thread = Thread(target=scheduler.run)
     scheduler_thread.start()
     # time.sleep(10) # wait for 10 seconds for example
